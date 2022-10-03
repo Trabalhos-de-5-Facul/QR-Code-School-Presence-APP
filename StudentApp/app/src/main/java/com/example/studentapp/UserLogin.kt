a99965529d2dd07e28b9683513795967bdfbda0f
Henrique Dias <hvdias2015@gmail.com>
2022-10-03 01:51:22 -0300

    Verifying Student Credentials at API

--- StudentApp/app/src/main/java/com/example/studentapp/UserLogin.kt
+++ StudentApp/app/src/main/java/com/example/studentapp/UserLogin.kt
@@ -3,13 +3,29 @@ package com.example.studentapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
-import android.view.View
-import android.widget.EditText
 import com.google.android.material.button.MaterialButton
+import io.ktor.client.*
+import io.ktor.client.call.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.plugins.kotlinx.serializer.*
+import io.ktor.client.plugins.json.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import kotlinx.coroutines.CoroutineStart
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+import org.json.JSONObject
+import java.lang.Thread.sleep
+import java.net.URL
+
 const val EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE"
 
 class UserLogin : AppCompatActivity() {
+    private var responseHttp = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.user_login_activity)
@@ -21,12 +37,23 @@ class UserLogin : AppCompatActivity() {
         //Admin Admin
 
         loginBtn.setOnClickListener {
-            //your implementation goes here
-            if(username.text.toString() == "Admin" && password.text.toString() == "Admin"){
+            val usernameValue = username.text.toString();
+            val passwordValue = password.text.toString();
+            responseHttp = ""
+            GlobalScope.async{
+                getLoginResponse(
+                "http://54.94.139.104:3000/alunos/$passwordValue/$usernameValue"
+                )
+            }
+
+            do{
+                sleep(1000)
+            }while (responseHttp == "")
+            val json = JSONObject(responseHttp)
+            if(responseHttp != "{\"erro\":\"Email ou Senha incorretos.\"}"){
                 //correct
-                val message = "{" + username.text.toString() + "," + password.text.toString() + "}"
                 val intent = Intent(this, QRCodeReader::class.java).apply {
-                    putExtra(EXTRA_MESSAGE, message)
+                    putExtra(EXTRA_MESSAGE, responseHttp)
                 }
                 startActivity(intent)
             }else{
@@ -34,5 +61,16 @@ class UserLogin : AppCompatActivity() {
 
             }
         }
+    }
+
+    private suspend fun getLoginResponse(httpString: String) {
+        val client = HttpClient(CIO) {
+            install(JsonPlugin) {
+                serializer = KotlinxSerializer()
+            }
+        }
+        val response: HttpResponse = client.get(httpString)
+        responseHttp = response.bodyAsText()
+        client.close()
     }
 }
