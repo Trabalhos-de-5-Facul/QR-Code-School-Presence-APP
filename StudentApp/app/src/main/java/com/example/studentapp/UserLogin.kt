208a849b5e911d99c3c76f1fd5aea13fba7601f6
luckytods <lucasrsj.miguel@gmail.com>
2022-11-04 14:19:34 -0300

    Main menu card font and text collor altered and one time login implemented(logout not working yet)

--- StudentApp/app/src/main/java/com/example/studentapp/UserLogin.kt
+++ StudentApp/app/src/main/java/com/example/studentapp/UserLogin.kt
@@ -5,8 +5,12 @@ import android.content.Intent
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.preference.PreferenceManager
 import android.util.Log
+import android.view.View
 import android.widget.TextView
+import com.example.studentapp.UserLogin.PreferenceHelper.customPreference
+import com.example.studentapp.UserLogin.PreferenceHelper.userId
 import com.google.android.material.button.MaterialButton
 import io.ktor.client.*
 import io.ktor.client.call.*
@@ -25,17 +29,23 @@ import java.net.URL
 
 const val EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE"
 
+
 class UserLogin : AppCompatActivity() {
-    
+
+    val CUSTOM_PREF_NAME = "User_data"
 
     private var responseHttp = ""
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        val prefs = customPreference(this, CUSTOM_PREF_NAME)
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.user_login_activity)
 
         val username = findViewById<TextView>(R.id.username)
         val password = findViewById<TextView>(R.id.password)
         val loginBtn = findViewById<MaterialButton>(R.id.login_btn)
+
 
         //Admin Admin
 
@@ -55,6 +65,8 @@ class UserLogin : AppCompatActivity() {
             val json = JSONObject(responseHttp)
             if(responseHttp != "{\"erro\":\"Email ou Senha incorretos.\"}"){
                 //correct
+
+                prefs.userId = usernameValue
                 val intent = Intent(this, QRCodeReader::class.java).apply {
                     putExtra(EXTRA_MESSAGE, responseHttp)
                 }
@@ -64,6 +76,35 @@ class UserLogin : AppCompatActivity() {
 
             }
         }
+    }
+
+    object PreferenceHelper {
+
+        val USER_ID = "USER_ID"
+
+        fun customPreference(context: Context, name: String): SharedPreferences = context.getSharedPreferences(name, Context.MODE_PRIVATE)
+
+        inline fun SharedPreferences.editMe(operation: (SharedPreferences.Editor) -> Unit) {
+            val editMe = edit()
+            operation(editMe)
+            editMe.apply()
+        }
+
+        var SharedPreferences.userId
+            get() = getString(USER_ID, "")
+            set(value) {
+                editMe {
+                    it.putString(USER_ID, value)
+                }
+            }
+
+        var SharedPreferences.clearValues
+            get() = { }
+            set(value) {
+                editMe {
+                    it.clear()
+                }
+            }
     }
 
     private suspend fun getLoginResponse(httpString: String) {
